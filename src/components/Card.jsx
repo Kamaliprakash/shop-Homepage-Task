c6ed9c29b581b293d16d4069dca3cf4ebd5d56ca
Kamaliprakash <kamaliprakash0307@gmail.com>
2024-01-01 13:43:37 +0530

    image update

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -15,7 +15,7 @@ function Card({ cart, setCart,e }) {
 
         <img
           className="card-img-top"
-          src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg"
+          src={e.image || "https://dummyimage.com/450x300/dee2e6/6c757d.jpg"}
           alt="..."
         />
 
